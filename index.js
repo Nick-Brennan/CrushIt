f600f9aee2553ebb5bc2f4c5987678a2dd6d59fc
Nick Brennan <nick_p_b@yahoo.com>
2016-02-05 14:31:31 -0800

    added users and configured passport

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
+var session = require('express-session');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt-nodejs');
+var nodemailer = require('nodemailer');
+var async = require('async');
+var crypto = require('crypto');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-
+var db = require('./models');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,13 +24,49 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({secret: 'hide this later'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//passport configuration
+passport.use(new Strategy(function(username, password, done){
+  db.User.findOne({username: username}, function(err, user){
+    if(err) return done(err);
+    if(!user) return done(null, false, {message: "Wrong Username"});
+    user.comparePassword(password, function(err, isMatch){
+      if(isMathch){
+        return doe(null, user);
+      }else{
+        return done(null, false, {message: "Wrong Password"});
+      }
+    });
+  });
+}));
 
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  db.User.findById(id, function(err, user){
+    done(err, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //routes
 app.get('/', function(req, res){
-  res.render('index', {title: 'Crush'});
+  res.render('index', {
+    title: 'Crush',
+    user: req.user
+  });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', {
+    user: req.user
+  });
 });
 
 
---
+++ models/index.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+mongoose.connect(	process.env.MONGOLAB_URI ||
+					process.env.MONGOHQ_URL ||
+					"mongodb://localhost/CrushIt");
+
+module.exports.User = require('./user.js');
