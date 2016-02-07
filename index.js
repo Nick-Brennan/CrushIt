77605a74db3832ee62594ce4016fbaa024be1467
Nick Brennan <nick_p_b@yahoo.com>
2016-02-07 00:36:32 -0800

    added flash message on login fail

--- index.js
+++ index.js
@@ -131,7 +131,8 @@ app.post('/login', function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
     if (err) return next(err)
     if (!user) {
-      return res.redirect('/login')
+      req.flash('error', 'Incorrect Username or Password');
+      return res.redirect('/login');
     }
     req.logIn(user, function(err) {
       if (err) return next(err);
